ffff9a870a172db38684e98489bc03dc554a72e2
michael <michael-haroon@users.noreply.github.com>
2025-06-04 13:57:34 -0700

    added universality/security in backend server application

--- backend/connect.js
+++ backend/connect.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "PleaseStopForgetting",
-  database: "social",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
