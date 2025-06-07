0744f6d54ed87c34f0a1375d0103e088a87b0d85
michael <michael-haroon@users.noreply.github.com>
2025-06-07 14:20:36 -0700

    cleaned up backend

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
-import likeRoutes from "./routes/likes.js";
-import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
 import multer from "multer";
 import cookieParser from "cookie-parser";
@@ -45,8 +43,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
-app.use("/api/likes", likeRoutes);
-app.use("/api/relationships", relationshipRoutes);
 
 
 app.listen(8800, () => {
